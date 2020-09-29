6a22c411b77e15080b02fe6f942432232184017c
Henrique Coppola <hvenses@gmail.com>
2020-09-29 19:45:49 -0300

    Comitando os exercicios 1 ao 4 da segunda parte.

---
+++ modulo_1/bloco_7/7_3/ex02_02.js
@@ -0,0 +1,16 @@
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
