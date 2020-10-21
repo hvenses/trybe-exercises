21cd4b6b20c31ee1aef848b6bba841cee058a9d8
Henrique Coppola <hvenses@gmail.com>
2020-10-21 15:21:33 -0300

    atualizando commits bloco8

--- modulo_1/bloco_8/8_1/exercise1.js
+++ modulo_1/bloco_8/8_1/exercise1.js
@@ -68,6 +68,6 @@ const books = [
 function authorBornIn1947() {
   // escreva aqui o seu código
   return books.find(book => book.author.birthYear === 1947).author.name;
-}
+  }
 
 assert.equal(authorBornIn1947(), 'Stephen King');
