1ba9f2c970439a9a7662a203ecd37b0c23c31d31
Henrique Coppola <hvenses@gmail.com>
2020-10-29 12:35:21 -0300

    Primeiro commit dos testes com Jest

---
+++ modulo_1/bloco_10/10_1/myRemove.test.js
@@ -0,0 +1,21 @@
+const myRemove = require('./myRemove');
+
+describe('myRemove', () => { 
+  test('Retorna um array sem o item que esta fora da matriz', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+
+  test('Retorna um array diferente dos itens dentro da matriz', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  })
+
+  test('Verifica se o array padrao nao sofreu alteração', () => {
+    const arr = [1, 2, 3, 4];
+    myRemove(arr, 1);
+    expect(arr).toEqual([1, 2, 3, 4]);
+  })
+
+  test('Retorna um array identico se o item nao pertence a ele', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
--- modulo_1/bloco_10/10_1/sum.test.js
+++ modulo_1/bloco_10/10_1/sum.test.js
@@ -1,5 +1,23 @@
-const sum = (a, b) => a + b;
+const sum = require('./sum');
 
-test('sums two values', () => {
-  expect(sum(2, 3)).toEqual(5);
+describe('sum', () => {
+  test('4 somado a 5 igual a 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  test('Erro quando for informado uma string', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow();
+  });
+
+  test('Zero somado a zero igual a zero', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  test('Mensagem de erro "parametros precisam ser numeros"', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow(/parameters must be numbers/);
+  });
 });
