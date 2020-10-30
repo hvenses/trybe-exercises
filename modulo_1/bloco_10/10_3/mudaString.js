e6bb964fd89bf8f5d0c80fccabd26dbd92ae12d6
Henrique Coppola <hvenses@gmail.com>
2020-10-30 19:54:26 -0300

    Alterando o moduleExports retirando do objeto

--- modulo_1/bloco_10/10_3/mudaString.js
+++ modulo_1/bloco_10/10_3/mudaString.js
@@ -2,8 +2,5 @@ const mudaString = (string) => string.toUpperCase();
 const primeiraLetra = (string) => string[0];
 const concatString = (s1, s2) => s1.concat(s2);
 
-module.exports = {
-  mudaString, 
-  primeiraLetra, 
-  concatString, 
-}
+module.exports =  mudaString, primeiraLetra, concatString;
+
